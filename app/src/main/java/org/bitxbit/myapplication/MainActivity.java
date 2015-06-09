0c74492e61d525cb842d3d33f640087490a7725a
agore <agore@gannett.com>
2015-06-09 12:00:39 -0400

    more smooth animation

--- app/src/main/java/org/bitxbit/myapplication/MainActivity.java
+++ app/src/main/java/org/bitxbit/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package org.bitxbit.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,7 +17,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int HEADER_HEIGHT = 480;
-    private static final int OFFSET = 100
+    private static final int OFFSET = 120
             ;
 
     private RecyclerView rv;
@@ -40,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(llm);
         rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
             private int totalDy;
+            private int total = HEADER_HEIGHT / 2;
             private boolean blurbHidden;
             private boolean titleSmall;
             private boolean titleHidden;
@@ -55,36 +57,33 @@ public class MainActivity extends AppCompatActivity {
                 super.onScrolled(recyclerView, dx, dy);
                 totalDy += dy;
                 scrollingUp = (dy > 0);
-                System.out.println("totalDy " + totalDy);
+
                 if (scrollingUp) {
-                    if (!titleHidden && !blurbHidden && totalDy > HEADER_HEIGHT / 2) {
+                    if (totalDy > HEADER_HEIGHT / 2) {
                         adapter.hideHeader();
-                        adapter.hideBlurbTextView();
                         toolbarTitle.setText("SECTION");
-                        titleHidden = blurbHidden = true;
                     }
 
-                    if (!titleSmall && !titleHidden && totalDy > ((HEADER_HEIGHT / 3) - OFFSET)) {
-                        adapter.makeHeaderSmall();
-                        titleSmall = true;
+                    if (totalDy > 0) {
+                        adapter.alphaBlurbText(totalDy, total);
+                        adapter.scaleHeaderText(totalDy, total);
                     }
                 } else {
-                    if (blurbHidden && titleHidden && totalDy < (HEADER_HEIGHT - (HEADER_HEIGHT / 3)) ) {
-                        adapter.showBlurbTextView();
+                    if (totalDy < (HEADER_HEIGHT / 2)) {
                         adapter.showHeader();
                         toolbarTitle.setText("");
-                        adapter.makeHeaderSmall();
-                        blurbHidden = titleHidden = false;
+                        adapter.alphaBlurbText(totalDy, total);
+                        adapter.scaleHeaderText(totalDy, total);
                     }
 
-                    if (titleSmall && !titleHidden && totalDy < HEADER_HEIGHT / 2) {
-                        adapter.makeHeaderRegular();
-                        titleSmall = false;
+                    if (totalDy == 0) {
+                        adapter.scaleHeaderText(totalDy, total);
+                        adapter.showBlurbTextView();
                     }
                 }
             }
         });
-        adapter = new SectionHeaderAnimationAdapter();
+        adapter = new SectionHeaderAnimationAdapter(getApplicationContext());
         rv.setAdapter(adapter);
     }
 
@@ -98,8 +97,15 @@ public class MainActivity extends AppCompatActivity {
 
         private static final int HEADER = 0;
         private static final int TEXT = 2;
+        private static final int MAX_HEADER_TEXT_SIZE = 32;
+        private static final int MIN_HEADER_TEXT_SIZE = 14;
+        private Context context;
         private TextView blurbTextView;
         private TextView headerTextView;
+
+        public SectionHeaderAnimationAdapter(Context context) {
+            this.context = context;
+        }
 
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
@@ -157,11 +163,22 @@ public class MainActivity extends AppCompatActivity {
         public void hideHeader() {
             headerTextView.setVisibility(View.INVISIBLE);
         }
-        public void makeHeaderSmall() {
-            headerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+
+        public void scaleHeaderText(int totalDy, int total) {
+            float scale = scaleNumber(totalDy, total) * (MAX_HEADER_TEXT_SIZE - MIN_HEADER_TEXT_SIZE);
+            headerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, MAX_HEADER_TEXT_SIZE - scale);
         }
+
+        public void alphaBlurbText(int totalDy, int total) {
+            blurbTextView.setAlpha(1 - scaleNumber(totalDy, total));
+        }
+
         public void makeHeaderRegular() {
-            headerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32);
+            headerTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, MAX_HEADER_TEXT_SIZE);
+        }
+
+        public float scaleNumber(int totalDy, int total) {
+            return ((float) totalDy / total);
         }
     }
 
@@ -181,5 +198,10 @@ public class MainActivity extends AppCompatActivity {
             tvBlurb = (TextView) itemView.findViewById(R.id.txt_header_line2);
             tvHeader = (TextView) itemView.findViewById(R.id.txt_header_line1);
         }
+    }
+
+    public static float pixelsToSp(Context context, float px) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+        return px/scaledDensity;
     }
 }
